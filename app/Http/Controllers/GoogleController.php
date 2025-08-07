7836fe32dd7f63808039b1976602386ec6965ad6
Jeff <jeffcayabyab168@gmail.com>
2025-08-07 09:54:51 +0800

    google login handler

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle(Request $request)
+    {
+        return Socialite::driver('google')->redirect();
+    }   
+
+    public function handleGoogleCallback(Request $request)
+    {
+        $user = Socialite::driver('google')->user();
+        $findUser = User::where("google_id", $user->id)->first();
+
+        if(!is_null($findUser)){
+            Auth::login($findUser);
+        }else{
+            $user = User::create([
+                "name" => $user->name,
+                "email" => $user->email,
+                "google_id" => $user->id,
+                "password" => encrypt("password"),
+            ]);
+        }
+        return redirect("dashboard");
+    }   
+}
