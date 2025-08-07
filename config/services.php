2b3a101630d0ad78c8b6871078067b9b7476386e
Jeff <jeffcayabyab168@gmail.com>
2025-08-07 09:53:54 +0800

    google client id

--- config/services.php
+++ config/services.php
@@ -30,6 +30,11 @@ return [
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 
     'resend' => [
         'key' => env('RESEND_KEY'),
