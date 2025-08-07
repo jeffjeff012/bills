b54a7d9cb4ae490e81c1fea9a36f335a16a3325b
Jeff <jeffcayabyab168@gmail.com>
2025-08-07 09:54:37 +0800

    fixes the query, transfer to controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Note;
+
+class AdminController extends Controller
+{
+    public function dashboard()
+    {
+        $userCount = Schema::hasTable('users') ? User::count() : 0;
+        $noteCount = Schema::hasTable('notes') ? Note::count() : 0;
+        $totalLikes = Schema::hasTable('notes') ? Note::sum('likes') : 0;
+        $totalDislikes = Schema::hasTable('notes') ? Note::sum('dislikes') : 0;
+
+        return view('admin.dashboard', compact(
+            'userCount',
+            'noteCount',
+            'totalLikes',
+            'totalDislikes'
+        ));
+    }
+}
