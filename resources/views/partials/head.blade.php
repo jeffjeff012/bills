1567dea2b14ddbacd644ab876de630b9ee348d88
Jeff <jeffcayabyab168@gmail.com>
2025-08-28 13:04:42 +0800

    change the logo

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,8 +10,8 @@
     crossorigin="anonymous"
     referrerpolicy="no-referrer"
 />
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+<link rel="icon" href="/images/final.png" sizes="any">
+<link rel="icon" href="/images/final.png" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <link rel="preconnect" href="https://fonts.bunny.net">
