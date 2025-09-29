82fb79141e247510fc09d8463ef53a254a7a2020
Jeff <jeffcayabyab168@gmail.com>
2025-09-29 16:29:15 +0800

    hide and unhide comments

--- resources/views/livewire/comment-section.blade.php
+++ resources/views/livewire/comment-section.blade.php
@@ -29,7 +29,6 @@
             <p class="font-semibold">{{ session('success') }}</p>
         </div>
     @endif
-    {{-- <x-toast on="toast" /> --}}
     <div>
         @forelse($comments as $comment)
             <div class="p-2 border-b text-xs sm:text-lg  {{ $comment->is_hidden ? 'opacity-60' : '' }}">
@@ -99,14 +98,14 @@
                                             ? 'bg-green-100 text-green-700 hover:bg-green-200 border border-green-300' 
                                             : 'bg-gray-100 text-gray-700 hover:bg-gray-200 border border-gray-300' }}">
                                 @if($comment->is_hidden)
-                                    <svg class="w-6 h-6 text-green-800 dark:text-white mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <svg class="w-6 h-6 text-green-800 dark:text-green-800 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"/>
                                     <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
                                     </svg>
 
                                     Unhide
                                 @else
-                                    <svg class="w-6 h-6 text-gray-800 dark:text-white mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <svg class="w-6 h-6 text-gray-800 dark:text-blue-800 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="m4 15.6 3.055-3.056A4.913 4.913 0 0 1 7 12.012a5.006 5.006 0 0 1 5-5c.178.009.356.027.532.054l1.744-1.744A8.973 8.973 0 0 0 12 5.012c-5.388 0-10 5.336-10 7A6.49 6.49 0 0 0 4 15.6Z"/>
                                     <path d="m14.7 10.726 4.995-5.007A.998.998 0 0 0 18.99 4a1 1 0 0 0-.71.305l-4.995 5.007a2.98 2.98 0 0 0-.588-.21l-.035-.01a2.981 2.981 0 0 0-3.584 3.583c0 .012.008.022.01.033.05.204.12.402.211.59l-4.995 4.983a1 1 0 1 0 1.414 1.414l4.995-4.983c.189.091.386.162.59.211.011 0 .021.007.033.01a2.982 2.982 0 0 0 3.584-3.584c0-.012-.008-.023-.011-.035a3.05 3.05 0 0 0-.21-.588Z"/>
                                     <path d="m19.821 8.605-2.857 2.857a4.952 4.952 0 0 1-5.514 5.514l-1.785 1.785c.767.166 1.55.25 2.335.251 6.453 0 10-5.258 10-7 0-1.166-1.637-2.874-2.179-3.407Z"/>
@@ -118,9 +117,8 @@
                         </div>
                     @endif
 
-
                     {{-- Delete confirmation modal --}}
-                    <flux:modal name="delete-comment" class="min-w-[22rem]" wire:model="confirmingCommentDeletion">
+                    <flux:modal name="delete-comment" class="min-w-[10rem] lg:min-w-[22rem]" wire:model="confirmingCommentDeletion">
                         <div class="space-y-6">
                             <div>
                                 <flux:heading size="lg">Delete Comment?</flux:heading>
